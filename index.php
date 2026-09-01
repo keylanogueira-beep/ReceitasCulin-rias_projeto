a7fa7086a238be7796ee910229e86542d24394a5
keyla lima <keyla.nogueira@ba.estudante.senai.br>
2026-09-01 12:33:03 -0300

    Adiciona arquivos e configuracao do projeto

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+// Exemplo de uso dos seus Namespaces:
+// $db = new Config\configuration();
+// $controller = new Controller\ImcController();
